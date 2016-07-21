226f21a04673585aa2145c1bedc78e2bf39df689
cfchen <cfchen@futuredial.com>
2016-07-21 14:39:49 +0800

    Add fork version tag.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='dj-sso-server2',
     version=__version__,
-    packages=['djssoserver2'],
+    packages=['djssoserver'],
     include_package_data=True,
     license='GPL v2.0',
     description='A Django SSO provider application',
